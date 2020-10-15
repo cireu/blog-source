d6aa408e099c75103fb171221bac6230a398c79f
Zhu Zihao <all_but_last@163.com>
2020-10-15 14:57:22 +0800

    chore: Fix broken CI

--- etc/deploy-gitlab.sh
+++ etc/deploy-gitlab.sh
@@ -2,18 +2,16 @@
 
 set -euo pipefail
 
-mkdir ../gitlab-checkout
-
 ORIGIN_DIR=${PWD}
+
+git clone --depth=50 \
+    "https://citreu:${TOKEN}@gitlab.com/citreu/citreu.gitlab.io" \
+    ../gitlab-checkout
 
 pushd ../gitlab-checkout > /dev/null
 
-git init
 git config user.name "Zhu Zihao"
 git config user.email "all_but_last@163.com"
-git remote add origin "https://citreu:${TOKEN}@gitlab.com/citreu/citreu.gitlab.io"
-
-git fetch --no-recurse-submodules
 
 rm -rf public
 cp -r ${ORIGIN_DIR}/docs/.vuepress/dist ./public
@@ -23,7 +21,7 @@ git add --all .
 if git commit \
        -m "Deploying to Gitlab pages from ${GITHUB_SHA}." \
        --quiet --no-verify; then
-    git push origin master --quiet
+    git push origin --quiet
 fi
 
 popd > /dev/null
